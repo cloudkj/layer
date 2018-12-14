c0a2200a055bb0257b100e6a97fabab3097cf23f
cloudkj <cloudkj@users.noreply.github.com>
2018-12-14 00:37:42 -0800

    Update sample script

--- sample_cifar10_conv.sh
+++ sample_cifar10_conv.sh
@@ -2,5 +2,6 @@
 set -ex
 
 cat sample/cifar10_x.csv \
-    | csi -script conv.scm -w weights.keras_cifar10_cnn.0 -b biases.keras_cifar10_cnn.0 --input-shape=32,32,3  --filter-shape=3,3 --num-filters=10 -a relu \
-    | csi -script conv.scm -w weights.keras_cifar10_cnn.1 -b biases.keras_cifar10_cnn.1 --input-shape=30,30,10 --filter-shape=3,3 --num-filters=5  -a relu
+    | csi -script conv.scm -w weights.keras_cifar10_cnn.0 -b biases.keras_cifar10_cnn.0 --input-shape=32,32,3  --filter-shape=3,3 --num-filters=32 -a relu \
+    | csi -script conv.scm -w weights.keras_cifar10_cnn.1 -b biases.keras_cifar10_cnn.1 --input-shape=30,30,32 --filter-shape=3,3 --num-filters=32 -a relu \
+    | csi -script pool.scm --input-shape=28,28,32 --filter-shape=2,2 --stride=2 -p max
