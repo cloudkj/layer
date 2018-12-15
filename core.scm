edce342decfcd9a6b05eac3e76d67998a14720ce
cloudkj <cloudkj@users.noreply.github.com>
2018-12-14 16:22:20 -0800

    Add logging utility functions

--- core.scm
+++ core.scm
@@ -3,27 +3,6 @@
 (use blas input-parse)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; Debugging
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-;; Prints the 2D representation of a list of values as an h-by-w matrix
-(define (print-2d v h w)
-  (let loop ((vals v)
-             (i 0)
-             (j 0)
-             (s ""))
-    (cond ((>= i h) (print s))
-          ((>= j w) (loop vals (+ i 1) 0 (string-append s "\n")))
-          (else (loop (cdr vals) i (+ j 1)
-                      (string-append s " "
-                                     (if (= (car vals) 0.0)
-                                         "_"
-                                         (number->string
-                                          (modulo
-                                           (inexact->exact (floor (* 10 (car vals))))
-                                           10)))))))))
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Options handling
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
---
+++ util.scm
@@ -0,0 +1,33 @@
+;;; Utility functions
+
+(declare (unit util))
+
+;;; Logging
+
+;; Logs a varadic list of values by printing to stderr
+(define (log . args)
+  (let ((port (current-error-port)))
+    (let loop ((a args))
+      (if (= (length a) 0)
+          (newline port)
+          (begin
+            (display (car a) port)
+            (display " " port)
+            (loop (cdr a)))))))
+
+;; Logs the 2D representation of a list of numeric values as an h-by-w matrix
+(define (log-list-as-matrix v h w)
+  (let loop ((vals v)
+             (i 0)
+             (j 0)
+             (s ""))
+    (cond ((>= i h) (print s))
+          ((>= j w) (loop vals (+ i 1) 0 (string-append s "\n")))
+          (else (loop (cdr vals) i (+ j 1)
+                      (string-append s " "
+                                     (if (= (car vals) 0.0)
+                                         "_"
+                                         (number->string
+                                          (modulo
+                                           (inexact->exact (floor (* 10 (car vals))))
+                                           10)))))))))
