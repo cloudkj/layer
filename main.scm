3d6c2e32fa428d8d1c993b3798e5e8021661c35e
cloudkj <cloudkj@users.noreply.github.com>
2018-12-18 22:10:50 -0800

    Refactor options

--- main.scm
+++ main.scm
@@ -2,27 +2,54 @@
 
 (use getopt-long)
 
-(define command-options
-  (list (cons "dense" weighted-layer-options)
-        (cons "conv" conv-layer-options)
-        (cons "pool" pool-layer-options)))
+(define program "main")
 
-(define command-functions
-  (list (cons "dense" dense)
-        (cons "conv" conv)
-        (cons "pool" pool)))
+(define commands
+  (list (list "dense" weighted-layer-options dense)
+        (list "conv"  conv-layer-options     conv)
+        (list "pool"  pool-layer-options     pool)))
 
-(define (usage #!optional error)
-  (print "TODO: usage")
-  (when error (print error)))
+(define (print-command-usage command #!optional error)
+  (when error (print error))
+  (format #t "Usage: ~A ~A [options]\n\n" program command)
+  (print "Options:")
+  (print (usage (cadr (assoc command commands)))))
+
+(define (print-usage #!optional error)
+  (when error (begin (print error) (newline)))
+  (format #t "Usage: ~A command [options]\n\n" program)
+  (print "Commands:"))
+
+(define (parse-options command args grammar)
+  (handle-exceptions
+   e
+   (begin
+     ;; TODO: figure out how to print original error message
+     (print-command-usage command
+                          (format #f
+                                  "Error: ~A - ~A\n"
+                                  (get-condition-property e 'exn 'message)
+                                  (get-condition-property e 'exn 'arguments)))
+     #f)
+   (getopt-long args grammar)))
+
+(define (help? arg)
+  (member arg (list "-h" "--help")))
 
 (let* ((args (command-line-arguments))
        (command (if (= (length args) 0) #f (car args)))
-       (grammar (assoc command command-options)))
-  (cond ((not command) (usage))
-        ((not grammar) (usage "Invalid command"))
+       (def (assoc command commands)))
+  (cond ((not command)                                 ;; No given command
+         (print-usage))
+        ((help? command)                               ;; General usage
+         (print-usage))
+        ((not def)                                     ;; No matching command
+         (print-usage (format #f "'~A' is not a valid command" command)))
+        ((and (> (length args) 1) (help? (cadr args))) ;; Command usage
+         (print-command-usage command))
         (else
-         (let* ((options (getopt-long args (cdr grammar)))
-                (lookup (make-options-lookup options))
-                (f (cdr (assoc command command-functions))))
-           (f lookup)))))
+         (let* ((grammar (cadr def))
+                (f (caddr def))
+                (options (parse-options command args grammar)))
+           (when options
+                 (f (make-options-lookup options)))))))
--- options.scm
+++ options.scm
@@ -3,49 +3,53 @@
 ;;; Definitions of required parameters
 
 (define filter-shape-option
-  '(filter-shape "Filter shape"      (required #t) (value #t)))
+  '(filter-shape "Filter shape"      (required #t) (value (required "n,m,..."))))
 
 (define input-shape-option
-  '(input-shape "Input shape"        (required #t) (value #t)))
+  '(input-shape  "Input shape"       (required #t) (value (required "n,m,..."))))
 
 (define num-filters-option
-  '(num-filters "Number of filters"  (required #t) (value #t)))
+  '(num-filters  "Number of filters" (required #t) (value (required num))))
 
 (define weights-option
-  '(weights     "Weights file"       (required #t) (value #t) (single-char #\w)))
+  '(weights      "Weights file"      (required #t) (value (required filename)) (single-char #\w)))
 
 ;;; Definitions of optional parameters
-
 (define stride-option
-  '(stride      "Stride"             (required #f) (value #t) (single-char #\s)))
+  '(stride       "Stride"            (required #f) (value (required num))))
 
 (define biases-option
-  '(biases      "Biases file"        (required #f) (value #t) (single-char #\b)))
+  '(biases       "Biases file"       (required #f) (value (required filename)) (single-char #\b)))
 
 (define function-option
-  '(function    "Function"           (required #f) (value #t) (single-char #\f)))
+  '(function     "Function"          (required #f) (value (required name)) (single-char #\f)))
 
 ;;; Layer options
+
+(define (sort-options options)
+  (sort options
+        (lambda (a b) (string< (symbol->string (car a))
+                               (symbol->string (car b))))))
 
 (define layer-options
   (list input-shape-option))
 
 (define weighted-layer-options
-  (append layer-options
-          (list weights-option
-                biases-option
-                function-option)))
+  (sort-options (append layer-options
+                        (list weights-option
+                              biases-option
+                              function-option))))
 
 (define conv-layer-options
-  (append weighted-layer-options
-          (list filter-shape-option
-                num-filters-option)))
+  (sort-options (append weighted-layer-options
+                        (list filter-shape-option
+                              num-filters-option))))
 
 (define pool-layer-options
-  (append layer-options
-          (list filter-shape-option
-                function-option
-                stride-option)))
+  (sort-options (append layer-options
+                        (list filter-shape-option
+                              function-option
+                              stride-option))))
 
 ;;; Helpers
 
